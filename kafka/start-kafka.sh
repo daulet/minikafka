8d30b88d6bcf4583bb966caaeb69b69fe65abaa7
Daulet Zhanguzin <daulet@zhanguzin.kz>
2022-06-03 23:22:28 -0700

    Broker can be reached from outside of container; Lower Kafka version to 2.x to avoid issues with 3.x; Clean up /tmp in container to reduce image size to ~160MB;

--- kafka/start-kafka.sh
+++ kafka/start-kafka.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -e
 
+if [[ -z "$KAFKA_ADVERTISED_PORT" ]]; then
+    export KAFKA_ADVERTISED_PORT=9092
+fi
+
+if [[ -z "$KAFKA_BROKER_ID" ]]; then
+    export KAFKA_BROKER_ID=-1
+fi
+
+if [[ -z "$KAFKA_LOG_DIRS" ]]; then
+    if [[ $KAFKA_BROKER_ID != -1 ]]; then
+        export KAFKA_LOG_DIRS="/kafka/kafka-logs-$KAFKA_BROKER_ID"
+    else
+        export KAFKA_LOG_DIRS="/kafka/kafka-logs-$HOSTNAME"
+    fi
+fi
+
 if [[ -z "$KAFKA_PORT" ]]; then
     export KAFKA_PORT=9092
 fi
@@ -13,9 +29,8 @@ unset KAFKA_CREATE_TOPICS
     do
         env_var=$(echo "$VAR" | cut -d= -f1)
         
-        echo "Processing env var: $env_var"
-
         if [[ $env_var =~ ^KAFKA_ ]]; then
+            echo "Processing $env_var=${!env_var}"
             kafka_name=$(echo "$env_var" | cut -d_ -f2- | tr '[:upper:]' '[:lower:]' | tr _ .)
             echo "$kafka_name=${!env_var}" >> "./config/server.properties" # KAFKA_ZOOKEEPER_CONNECT
         fi
