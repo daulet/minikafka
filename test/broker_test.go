0d8b859c6faf7b3dea498e60b8566d5d43e38b01
Daulet Zhanguzin <daulet@zhanguzin.kz>
2022-06-24 10:44:07 -0700

    add test for end to end publish to subscriber on containers

--- test/broker_test.go
+++ test/broker_test.go
@@ -2,40 +2,44 @@ package test
 
 import (
 	"fmt"
+	"io"
 	"testing"
+	"time"
 
 	toxiproxy "github.com/Shopify/toxiproxy/v2/client"
 	"github.com/daulet/minikafka"
 )
 
+// TODO add bench equivalent to this test
 func TestWritesAreAcked(t *testing.T) {
 	var (
 		proxyPort = 9081
+		proxyAddr = fmt.Sprintf("toxiproxy:%d", proxyPort)
 		kafkaPort = 9091
-	)
-	client := toxiproxy.NewClient("toxiproxy:8474")
-	proxy, err := client.CreateProxy("kafka",
-		fmt.Sprintf("toxiproxy:%d", proxyPort),
-		fmt.Sprintf("minikafka:%d", kafkaPort),
-	)
-	if err != nil {
-		panic(err)
-	}
-	defer proxy.Delete()
+		kafkaAddr = fmt.Sprintf("minikafka:%d", kafkaPort)
 
+		msgs = 10
+	)
+	// setup proxy behavior
 	{
-		count := 10
-
+		client := toxiproxy.NewClient("toxiproxy:8474")
+		proxy, err := client.CreateProxy("kafka_pub", proxyAddr, kafkaAddr)
+		if err != nil {
+			panic(err)
+		}
+		defer proxy.Delete()
+	}
+	{
 		pub, err := minikafka.NewPublisher(
-			minikafka.PublisherBrokerAddress(fmt.Sprintf("toxiproxy:%d", proxyPort)),
+			minikafka.PublisherBrokerAddress(proxyAddr),
 		)
 		if err != nil {
 			t.Fatal(err)
 		}
 		defer pub.Close()
 
-		published := make(chan struct{}, count)
-		for i := 0; i < count; i++ {
+		published := make(chan struct{}, msgs)
+		for i := 0; i < msgs; i++ {
 			go func() {
 				err := pub.Publish("", []byte("Hello"))
 				if err != nil {
@@ -44,9 +48,130 @@ func TestWritesAreAcked(t *testing.T) {
 				published <- struct{}{}
 			}()
 		}
-		for i := 0; i < count; i++ {
+		for i := 0; i < msgs; i++ {
 			<-published
 		}
 		close(published)
+	}
+}
+
+func TestAllPublished(t *testing.T) {
+	var (
+		proxyPubPort = 9081
+		proxySubPort = 9082
+		kafkaPubPort = 9091
+		kafkaSubPort = 9092
+
+		subs = 3
+	)
+	// setup proxy behavior
+	{
+		client := toxiproxy.NewClient("toxiproxy:8474")
+		{
+			proxy, err := client.CreateProxy("kafka_pub",
+				fmt.Sprintf("toxiproxy:%d", proxyPubPort),
+				fmt.Sprintf("minikafka:%d", kafkaPubPort),
+			)
+			if err != nil {
+				panic(err)
+			}
+			defer proxy.Delete()
+		}
+		{
+			proxy, err := client.CreateProxy("kafka_sub",
+				fmt.Sprintf("toxiproxy:%d", proxySubPort),
+				fmt.Sprintf("minikafka:%d", kafkaSubPort),
+			)
+			if err != nil {
+				panic(err)
+			}
+			defer proxy.Delete()
+		}
+	}
+
+	expected := make(map[string]struct{})
+	for i := 0; i < 10; i++ {
+		expected[fmt.Sprintf("Hello\n%d", i)] = struct{}{}
+	}
+
+	// publish messages
+	{
+		pub, err := minikafka.NewPublisher(
+			minikafka.PublisherBrokerAddress(fmt.Sprintf("toxiproxy:%d", proxyPubPort)),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer pub.Close()
+
+		published := make(chan struct{}, len(expected))
+		for msg := range expected {
+			go func(s string) {
+				err := pub.Publish("", []byte(s))
+				if err != nil {
+					t.Errorf("error publishing message: %v", err)
+				}
+				published <- struct{}{}
+			}(msg)
+		}
+		for range expected {
+			<-published
+		}
+		close(published)
+	}
+	// multiple subscribers on the same topic
+	{
+		subCh := make(chan struct{}, subs)
+		for i := 0; i < subs; i++ {
+			exp := make(map[string]struct{})
+			for k, s := range expected {
+				exp[k] = s
+			}
+
+			go func(expected map[string]struct{}) {
+				sub, err := minikafka.NewSubscriber(
+					minikafka.SubscriberBrokerAddress(fmt.Sprintf("toxiproxy:%d", proxySubPort)),
+				)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+				defer sub.Close()
+
+				for {
+					bytes, err := sub.Read()
+					if err != nil {
+						if err == io.EOF {
+							break
+						}
+						t.Error(err)
+						return
+					}
+					msg := string(bytes)
+					if _, ok := expected[msg]; !ok {
+						t.Errorf("unexpected message received: %s", msg)
+						continue
+					}
+					delete(expected, msg)
+					if len(expected) == 0 {
+						break
+					}
+				}
+
+				for msg := range expected {
+					t.Errorf("expected message not received: %s", msg)
+				}
+				subCh <- struct{}{}
+			}(exp)
+		}
+		// subscribers exit when all expected messages are received
+		for i := 0; i < subs; i++ {
+			select {
+			case <-subCh:
+				continue
+			case <-time.After(time.Second):
+				t.Fatal("timeout waiting for subscriber to receive all expected messages")
+			}
+		}
 	}
 }
